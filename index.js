2c79cce2bd2e9a282197d748bd7dd74c4b34d60e
cdunsford <cdunsford@gmail.com>
2020-12-21 07:51:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*document.addEventListener('click', e => 
+{
+    e.target.innerHTML = '';
+})
+
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});*/
+
